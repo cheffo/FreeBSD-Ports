e1b31cc2f661a8ecd178821fdfd354c90356b450
cheffo <cheffo@freebsd-bg.org>
2016-01-19 18:27:29 +0200

    inflix - telegraf - initial port needs clean up mondgodb input does not work - removed from build process

---
+++ net-mgmt/telegraf/files/patch-plugins_inputs_all_all.go
@@ -0,0 +1,10 @@
+--- plugins/inputs/all/all.go.orig	2016-01-19 15:23:22 UTC
++++ plugins/inputs/all/all.go
+@@ -16,7 +16,6 @@ import (
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/lustre2"
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/mailchimp"
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/memcached"
+-	_ "github.com/influxdb/telegraf/plugins/inputs/mongodb"
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/mysql"
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/nginx"
+ 	_ "github.com/influxdb/telegraf/plugins/inputs/phpfpm"
